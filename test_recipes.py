7838573667c200ae0ddd255764ed0ce6eaf88ccb
Шамилов Эмин <efshamilov@edu.hse.ru>
2026-06-03 02:06:40 +0300

    сделал тесты для класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -17,3 +17,26 @@ def test_ingredient_eq():
     assert a == b
     assert a !=c
     assert a !=d
+def test_create_recipe():
+    recipe = Recipe("Блины")
+    assert recipe.title == "Блины"
+    assert recipe.ingredients == []
+def test_add_ingredient():
+    recipe = Recipe("Блины")
+    recipe.add_ingredient(Ingredient("Мука", 400, "гр"))
+    recipe.add_ingredient(Ingredient("Мука", 60, "гр"))
+    assert len(recipe) == 1
+    assert recipe.ingredients[0].quantity == 460.0
+def test_scale():
+    recipe = Recipe("Блины")
+    recipe.add_ingredient(Ingredient("Мука", 400, "гр"))
+    scaled = recipe.scale(2)
+    assert scaled is not recipe
+    assert scaled.ingredients[0].quantity == 800.0
+    with pytest.raises(ValueError):
+        recipe.scale(-1)
+def test_len():
+    recipe = Recipe("Блины")
+    recipe.add_ingredient(Ingredient("Мука", 340, "гр"))
+    recipe.add_ingredient(Ingredient("Молоко", 500, "мл"))
+    assert len(recipe) == 2
